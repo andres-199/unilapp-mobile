37aad3fa0c9736c9c84d8114a680ea93a83bc6b4
andres199 <calopez@asertisas.com>
2021-06-07 19:43:36 -0500

    forgotten password i

---
+++ src/app/login/forgotten-password/forgotten-password.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgotten-password',
+  templateUrl: './forgotten-password.page.html',
+  styleUrls: ['./forgotten-password.page.scss'],
+})
+export class ForgottenPasswordPage implements OnInit {
+  email: string;
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LogoutGuard } from '../guards/logout.guard';
 
 import { LoginPage } from './login.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginPage
-  }
+    component: LoginPage,
+  },
+  {
+    path: 'forgotten-password',
+    loadChildren: () =>
+      import('./forgotten-password/forgotten-password.module').then(
+        (m) => m.ForgottenPasswordPageModule
+      ),
+    canActivate: [LogoutGuard],
+  },
 ];
 
 @NgModule({
